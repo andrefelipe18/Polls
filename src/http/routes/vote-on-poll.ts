b52e9d143cc479bcef1296a95ed12e3ca8b62a71
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-02-06 12:06:53 -0300

    not accepted duplicate votes or votes in same option

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -19,18 +19,59 @@ export async function voteOnPoll(app: FastifyInstance) {
 
     let { sessionId } = request.cookies;
 
-    if (!sessionId) {
-      sessionId = randomUUID() + new Date().toISOString();
+    if (sessionId) {
+      const userPreviousVote = await p.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (userPreviousVote && userPreviousVote.pollOptionId !== pollOptionId) {
+        await p.vote.delete({
+          where: {
+            id: userPreviousVote.id,
+          },
+        });
+
+        const vote = await p.vote.create({
+          data: {
+            pollOptionId,
+            pollId,
+            sessionId,
+          },
+        });
+      } else if (userPreviousVote) {
+        return reply.code(400).send({
+          message: "User already voted",
+        });
+      }
     }
 
-    reply.setCookie("sessionId", sessionId, {
-      path: "/",
-      maxAge: 60 * 60 * 30, //30 dias
-      httpOnly: true,
-      signed: true,
+    if (!sessionId) {
+      sessionId = randomUUID() + new Date().toISOString();
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 30, //30 dias
+        httpOnly: true,
+        signed: true,
+      });
+    }
+
+    const vote = await p.vote.create({
+      data: {
+        pollOptionId,
+        pollId,
+        sessionId,
+      },
     });
 
-    reply.send({ sessionId });
+    return reply.code(201).send({
+      message: "Vote registered",
+    });
   });
 }
 
