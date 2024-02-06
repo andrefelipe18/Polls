5057158b480214e58dcce9aa938197cb574b10b0
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-02-06 11:46:54 -0300

    create votes system

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import p from "../../lib/prisma";
 import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (request, reply) => {
@@ -15,6 +16,21 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollId: z.string().uuid(),
       })
       .parse(request.params);
+
+    let { sessionId } = request.cookies;
+
+    if (!sessionId) {
+      sessionId = randomUUID() + new Date().toISOString();
+    }
+
+    reply.setCookie("sessionId", sessionId, {
+      path: "/",
+      maxAge: 60 * 60 * 30, //30 dias
+      httpOnly: true,
+      signed: true,
+    });
+
+    reply.send({ sessionId });
   });
 }
 
