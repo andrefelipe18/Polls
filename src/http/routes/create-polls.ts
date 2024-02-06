0cee42129a3c85abcb877e4d2970b99c4d604fca
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-02-06 11:17:07 -0300

    create poll finish

--- src/http/routes/create-polls.ts
+++ src/http/routes/create-polls.ts
@@ -6,18 +6,33 @@ export async function createPoll(app: FastifyInstance) {
   app.post("/polls", async (request, reply) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
 
-    const { title } = createPollBody.parse(request.body);
+    const { title, options } = createPollBody.parse(request.body);
 
     const poll = await p.poll.create({
       data: {
         title,
+        options: { // Cria as opções do poll no mesmo momento que o poll
+          createMany: {
+            data: options.map((option) => ({
+              title: option,
+            })),
+          },
+        },
       },
     });
 
+    await p.pollOption.createMany({
+      data: options.map((option) => ({
+        pollId: poll.id,
+        title: option,
+      })),
+    });
+
     return reply.code(201).send({
-      polldId: poll.id,
+      poll,
     });
   });
 }
